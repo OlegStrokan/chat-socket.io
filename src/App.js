fccd3d238baf43d861b8f0ec00341b5c3c1650df
olegstrokan <oleg14ua71@gmail.com>
2021-07-19 15:46:31 +0200

    final version

--- src/App.js
+++ src/App.js
@@ -3,62 +3,62 @@ import axios from 'axios';
 
 import socket from './socket';
 
-import reducer from './reducer';
+import reducer from './logic/reducer';
 import JoinBlock from './components/JoinBlock';
 import Chat from './components/Chat';
 
 function App() {
-    const [state, dispatch] = React.useReducer(reducer, {
-        joined: false,
-        roomId: null,
-        userName: null,
-        users: [],
-        messages: [],
+  const [state, dispatch] = React.useReducer(reducer, {
+    joined: false,
+    roomId: null,
+    userName: null,
+    users: [],
+    messages: [],
+  });
+
+  const onLogin = async (obj) => {
+    dispatch({
+      type: 'JOINED',
+      payload: obj,
     });
+    socket.emit('ROOM:JOIN', obj);
+    const { data } = await axios.get(`/rooms/${obj.roomId}`);
+    dispatch({
+      type: 'SET_DATA',
+      payload: data,
+    });
+  };
 
-    const onLogin = async (obj) => {
-        dispatch({
-            type: 'JOINED',
-            payload: obj,
-        });
-        socket.emit('ROOM:JOIN', obj);
-        const { data } = await axios.get(`/rooms/${obj.roomId}`);
-        dispatch({
-            type: 'SET_DATA',
-            payload: data,
-        });
-    };
+  const setUsers = (users) => {
+    dispatch({
+      type: 'SET_USERS',
+      payload: users,
+    });
+  };
 
-    const setUsers = (users) => {
-        dispatch({
-            type: 'SET_USERS',
-            payload: users,
-        });
-    };
+  const addMessage = (message) => {
+    dispatch({
+      type: 'NEW_MESSAGE',
+      payload: message,
+    });
+  };
 
-    const addMessage = (message) => {
-        dispatch({
-            type: 'NEW_MESSAGE',
-            payload: message,
-        });
-    };
+  React.useEffect(() => {
+    socket.on('ROOM:SET_USERS', setUsers);
+    socket.on('ROOM:NEW_MESSAGE', addMessage);
+  }, []);
 
-    React.useEffect(() => {
-        socket.on('ROOM:SET_USERS', setUsers);
-        socket.on('ROOM:NEW_MESSAGE', addMessage);
-    }, []);
+  window.socket = socket;
 
-    window.socket = socket;
-
-    return (
-        <div className="wrapper">
-            {!state.joined ? (
-                <JoinBlock onLogin={onLogin} />
-            ) : (
-                <Chat {...state} onAddMessage={addMessage} />
-            )}
-        </div>
-    );
+  return (
+    <div className="wrapper">
+      {!state.joined ? (
+        <JoinBlock onLogin={onLogin} />
+      ) : (
+        <Chat {...state} onAddMessage={addMessage} />
+      )}
+    </div>
+  );
 }
 
 export default App;
